477ec20ec41bbb436b037aa2ba98aa73c7a8617c
MateoServant <114737857+MateoServant@users.noreply.github.com>
2023-04-25 11:00:24 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,22 +1,19 @@
-<!DOCTYPE html>   <!-- page de formulaire pour la connexion-->
-<html lang="fr">
+<!DOCTYPE html>
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="css/componant/inscr.css">
+    <link rel="stylesheet" href="css/componant/connexion.css">
     <title>connection</title>
 </head>
 <body>
-    <header>
+    
 
 
-    </header>
-
-
-    <main>
+    <main >
+    <section class="ff">
     <div class="login-form">
              <?php 
                 if(isset($_GET['login_err']))
@@ -52,18 +49,18 @@
                 }
                 ?> 
         <section class="vh-100">
-            <div class="container-fluid">
+            <div class="containder-fluid">
               <div class="row">
                 <div class="col-sm-6 text-black">
           
                   <div class="px-5 ms-xl-4">
-                    <i class="fas fa-crow fa-2x me-3 pt-5 mt-xl-4" style="color: #709085;"></i><br><br><br>
+                    <i class="fas fa-crow fa-2x me-3 pt-5 mt-xl-4"></i><br><br><br>
                     <span class="h1 fw-bold mb-0">Cook Connect</span>
                   </div>
           
                   <div class="d-flex align-items-center  ms-xl-4 mt-5 pt-5 pt-xl-0">
           
-                  <form action="connexion.php" method="post">
+                  <form action="connexion.php" method="post" class="pb">
                 <h2 class="text-center">Connexion</h2>       
                 <div class="form-group">
                     <input type="email" name="email" class="form-control" placeholder="Email" required="required" autocomplete="off">
@@ -74,42 +71,17 @@
                 <div class="form-group">
                     <button type="submit" class="btn btn-primary btn-block">Connexion</button>
                 </div>   
+                <div>
+                <p class="btn btn-primary btn-block "><a class="a" href="inscription.php">Inscription</a></p>
+                </div>
             </form>
-            <p class="text-center"><a href="inscription.php">Inscription</a></p>
         </div>
-        <style>
-            .login-form {
-                width: 340px;
-                margin: 50px auto;
-            }
-            .login-form form {
-                margin-bottom: 15px;
-                background: #f7f7f7;
-                box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
-                padding: 30px;
-            }
-            .login-form h2 {
-                margin: 0 0 15px;
-            }
-            .form-control, .btn {
-                min-height: 38px;
-                border-radius: 2px;
-            }
-            .btn {        
-                font-size: 15px;
-                font-weight: bold;
-            }
-        </style>
+        
           </section>
 
   </div>
+  </section>
     </main>
-
-    <footer>
-
-
-
-    </footer>
 
     <script src="scripts/main.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
